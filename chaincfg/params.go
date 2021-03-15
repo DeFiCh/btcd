6948e5fee4bfaef2afc81f228621e8a9066d37cd
DeFiDev <defidev@cakedefi.com>
2021-03-15 12:48:17 +0300

    chaincfg/params.go: change TestNet3Params address prefixes from bitcoin values to defichain values

--- chaincfg/params.go
+++ chaincfg/params.go
@@ -480,18 +480,21 @@ var TestNet3Params = Params{
 
 	// Human-readable part for Bech32 encoded segwit addresses, as defined in
 	// BIP 173.
-	Bech32HRPSegwit: "tb", // always tb for test net
+	Bech32HRPSegwit: "tf",
 
 	// Address encoding magics
-	PubKeyHashAddrID:        0x6f, // starts with m or n
-	ScriptHashAddrID:        0xc4, // starts with 2
-	WitnessPubKeyHashAddrID: 0x03, // starts with QW
-	WitnessScriptHashAddrID: 0x28, // starts with T7n
-	PrivateKeyID:            0xef, // starts with 9 (uncompressed) or c (compressed)
+	PubKeyHashAddrID: 0xf,
+	ScriptHashAddrID: 0x80,
+
+	// Old witness prefixes. Commented, 'cause it's unused
+	// WitnessPubKeyHashAddrID: 0x03,
+	// WitnessScriptHashAddrID: 0x28,
+
+	PrivateKeyID: 0xef,
 
 	// BIP32 hierarchical deterministic extended key magics
-	HDPrivateKeyID: [4]byte{0x04, 0x35, 0x83, 0x94}, // starts with tprv
-	HDPublicKeyID:  [4]byte{0x04, 0x35, 0x87, 0xcf}, // starts with tpub
+	HDPublicKeyID:  [4]byte{0x04, 0x35, 0x87, 0xCF},
+	HDPrivateKeyID: [4]byte{0x04, 0x35, 0x83, 0x94},
 
 	// BIP44 coin type used in the hierarchical deterministic path for
 	// address generation.
